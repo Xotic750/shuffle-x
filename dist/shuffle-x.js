246da0e6a79049dda30381333754e69f42a27498
Graham Fairweather <xotic750@gmail.com>
2019-07-17 20:40:54 +0200

    :bookmark: v2.0.8
    Auto build

--- dist/shuffle-x.js
+++ dist/shuffle-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-17T17:35:24.130Z",
+  "date": "2019-07-17T18:40:49.020Z",
   "describe": "",
   "description": "Creates an array of shuffled values.",
   "file": "shuffle-x.js",
   "hash": "b67e72deaa7054842f1d",
   "license": "MIT",
-  "version": "2.0.7"
+  "version": "2.0.8"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
