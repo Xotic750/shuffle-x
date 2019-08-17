69335fab9997bf7c8895a1db8b2f1ab35d4f6062
Graham Fairweather <xotic750@gmail.com>
2019-08-17 23:55:35 +0200

    Maintenance

--- dist/shuffle-x.js
+++ dist/shuffle-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-17T12:33:44.004Z",
+  "date": "2019-08-17T21:55:29.037Z",
   "describe": "",
   "description": "Creates an array of shuffled values.",
   "file": "shuffle-x.js",
